fd4cf82f17abd3006a9dae4006a56075cb502c18
Oliver Falk <oliver.falk@omv.com>
2015-10-13 12:27:49 +0200

    Host as argument, fix URL

--- wasagent.sh
+++ wasagent.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-HOST="127.0.0.1"
+HOST=$1
 CONNECTOR="9090"
 
-DATA=$(wget -q -O - http://${HOST}:${CONNECTOR}/wasagent/WASAgent --post-data=$@ 2> /dev/null)
+DATA=$(wget -q -O - http://${HOST}:${CONNECTOR}/wasagent/WasAgent --post-data=$@ 2> /dev/null)
+
 [ $? != 0 ] && exit 2
 echo ${DATA} | awk -F\| '{ print $2"|"$3  ; exit $1 }'
 exit $?
